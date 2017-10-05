680eb5c91c43b1893f7a6b07f9457c52db0f9db5
tgzt <32170685+tgzt@users.noreply.github.com>
2017-10-05 14:16:02 -0400

    Lab release
    Mods for Cisco lab

--- bulkCreate.py
+++ bulkCreate.py
@@ -1,5 +1,11 @@
 import requests
 import xml.etree.ElementTree as ET
+#
+import json
+#
+usr = "ers"
+pwd = "ersM0"
+ise = "ise.secure-x.local"
 
 #Import XML file for NADs and make it useable
 xml = open('nad.xml', 'r')
@@ -8,14 +14,15 @@ xml3 = ET.fromstring(xml2)
 payload =  ET.tostring(xml3, encoding='utf-8', method='xml')
 
 #Start Bulk Push Creation
-url = "https://X.X.X.X:9060/ers/config/networkdevice/bulk/submit"
+url = "https://%s:9060/ers/config/networkdevice/bulk/submit" % ise
 
 headers = {
     'content-type': "application/vnd.com.cisco.ise.network.networkdevicebulkrequest.1.1+xml",
-    'authorization': "Basic XXXXXXXXXXXXXXXXXX",
+#    'authorization': "Basic XXXXXXXXXXXXXXXXXX",
     'cache-control': "no-cache"
 }
-response = requests.request("PUT", url, data=payload, headers=headers, verify=False)
+response = requests.request("PUT", url, data=payload, headers=headers, 
+    auth=(usr,pass), verify=False)
 
 #Response manipulation
 responselocation = response.headers['location']
@@ -25,11 +32,11 @@ bulkstatusid = responselocation.split("submit/",1)[1]
 print ("Bulk ID:" + str(bulkstatusid))
 
 #Start Bulk Status GET
-url = "https://X.X.X.X:9060/ers/config/networkdevice/bulk/" + str(bulkstatusid)
+url = "https://%s:9060/ers/config/networkdevice/bulk/" % ise + str(bulkstatusid)
 
 headers = {
     'content-type': "application/vnd.com.cisco.ise.network.networkdevicebulkstatus.1.1+xml",
-    'authorization': "Basic XXXXXXXXXXXXXXXXXX",
+ #   'authorization': "Basic XXXXXXXXXXXXXXXXXX",
     'cache-control': "no-cache"
 }
 
